eaa95a35fa5dfe02db605f6fecfd472c2178c96e
Yohello1 <mp2702737@gmail.com>
2020-12-01 22:48:06 -0500

    index temp fix

--- index.php
+++ index.php
@@ -1,20 +1,187 @@
-<!DOCTYPE html>
-<html lang="en" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <title>php</title>
-  </head>
-  <body>
+<head>
 
-    <?php
-    $name = "Sam";
-    $why = 167;
-    echo "<p> Someone named $name, is nice, $name has $why dollars</p>";
-    $name = "Joe";
-    echo "<p>John changed his name to $name</p>";
-$Date = date('m/d/Y');
-echo "<p> $Date </p>"
-    ?>
+    <meta charset="UTF-8">
 
-  </body>
-</html>
+    <title>Yohello</title>
+    <link rel="stylesheet" href="indexx.css">
+</head>
+
+<body>
+    <div class="start">
+        <br>
+        <br>
+        <br>
+        <h1>Yohello</h1>
+        <br>
+        <br>
+        <br>
+    </div>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <div class="row">
+
+        <div class="left-col">
+            <h2><a href="#Projects"><u>Projects</u></a></h2>
+
+        </div>
+
+        <div class="center-col">
+
+            <h2><a href="fullintroduction.html">About me</a></h2>
+
+        </div>
+
+        <div class="right-col">
+
+            <h2><a href="#Contact-info">Contact Me</a></h2>
+        </div>
+
+    </div>
+
+    <br>
+    <br>
+    <br>
+    <div class="container2">
+        <a href="fullintroduction.html">
+            <img src="Images/Tower.jpg" style="width:100%; height:100%">
+            <div class="text-block">
+                <h4>Arcade Machine</h4>
+                <p>HOW</p>
+            </div>
+        </a>
+    </div>
+    <div class="container3">
+        <img src="Images/untitled17.jpg" style="width:100%; height:100%">
+        <div class="text-block">
+            <h4>Arcade Machine</h4>
+            <p>HOW</p>
+        </div>
+    </div>
+    <div class="container4">
+        <img src="Images/Arcade1080.jpg" style="width:100%; height:100%">
+        <div class="text-block">
+            <h4>Arcade Machine</h4>
+            <p>HOW</p>
+        </div>
+    </div>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <h3>Test
+    </h3>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+
+
+    <h3 class="h31">About Me</h3>
+    <div class="rectangle"></div>
+    <div class="introtext">
+        <p> I'm Yohello from <i>the mythical land of Canada</i>, I am a human (I think), and I know a bit about computers As of now I live I'm in the Eastern Time Zone (-4 GMT, -5 UTC), and usally acesable around 3-4pm and 8-9pm on weekdays and weekends just
+            send me a message, I rarly know what is going on in advanced I am a human I have a life outside of the internet, I make errors, and take time to do things. I'm not taking job requets no matter who you are, I can redirect you to someone who
+            might be able to. Also I know a bit about how computers work but I'm no expert, I learned <b>All</b> of it online, and self taught. I've learned Cgi and Vfx with Blender, I started programing by learning HTML, then diving straight into C#
+            with Unity, and Python then I took a break for about a year and got back into coding with C, and that is how I got here.
+            <a href="fullintroduction.html">Proper introduction</a></p>
+    </div>
+    <br>
+    <div class="nextpoint">
+
+    </div>
+    <div class="cgivfxtext">
+        <h3>Cgi and Vfx</h3>
+        <div class="rectangle2"></div>
+        <p>I'm always working on some sort of cgi/vfx, this is my latest project involving fire,hardsurface,rigidbody, and so much more. <br> <a href="Projects/Visualfx.html"> To learn more about this project</a> <br> <a href="Projects/Projecthomepage.html">Projects in general</a>
+        </p>
+
+    </div>
+
+    <div class="imgvfx">
+        <img src="Images/Cannonshooting.jpg">
+    </div>
+
+
+
+
+    <div class="nextpoint">
+
+    </div>
+    <br>
+    <div class="cgivfxtext">
+        <h3>Coding</h3>
+        <div class="rectangle3"></div>
+        <p>I'm not always coding, but when I do I do quite a lot of coding. As in I'll make an entire website, app, or game<br> I've used Godot, Unreal, and Unity to Develop games. I use python when doing stuff with api's, and I use C/C++ when doing standalone
+            programs and such usally I'm using C. <br> <a href="https://github.com/Yohello1"> Github, Fraction of what I've done is saved here</a> <br> <a href="Projects/Coding.html">Coding Page</a> <br> <a href="Projects/Projecthomepage.html">All Projects</a>
+        </p>
+    </div>
+    <div class="imgvfx">
+        <img src="Images/code.jpg">
+    </div>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <div class="nextpoint">
+
+    </div>
+    <br>
+    <div class="Contactinfo">
+        <h3>Contact Information</h3>
+        <div class="rectangle4"></div>
+        <p>Insert Contact information.
+            <br> <br>I will not work for you.<br> This will doubtfully change
+        </p>
+    </div>
+    <div class="nextpoint"></div>
+    <br>
+    <br>
+    <br>
+
+</body>
