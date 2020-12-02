d2d73bd5dafd1ee86a5298f484779bfaa135c420
Yohello1 <mp2702737@gmail.com>
2020-12-01 22:29:11 -0500

    trying something

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>php</title>
+  </head>
+  <body>
+
+    <?php
+    $name = "Sam";
+    $why = 167;
+    echo "<p> Someone named $name, is nice, $name has $why dollars</p>";
+    $name = "Joe";
+    echo "<p>John changed his name to $name</p>";
+$Date = date('m/d/Y');
+echo "<p> $Date </p>"
+    ?>
+
+  </body>
+</html>
