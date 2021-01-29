046cac1dfc067a17b4fe07550c130b3309fb6ea8
Yohello1 <mp2702737@gmail.com>
2021-01-28 22:54:15 -0500

    trying

--- index.php
+++ index.php
@@ -3,14 +3,14 @@
     <meta charset="UTF-8">
 
     <title>Yohello</title>
-<--    <link rel="stylesheet" href="indexx.css">   -->
-    <link rel="shortcut icon" type="image/x-icon" href="favicon.ico" />
+    <-- <link rel="stylesheet" href="indexx.css"> -->
+        <link rel="shortcut icon" type="image/x-icon" href="favicon.ico" />
 </head>
 
 <?php
 
 $jsondata = file_get_contents("data.json");
-$json = json_decode($jsondata,true);
+$json = json_decode($jsondata, true);
 
 $name = "Yohello";
 $time = "not avaliable during this time";
@@ -27,232 +27,264 @@ $project2 = "Need to make";
 
 
 <body>
-<style>
+    <style>
+        @font-face {
+            font-family: FiraCode;
+            src: url(FiraCode-VariableFont_wght.ttf)
+        }
 
-@font-face {
-    font-family: FiraCode;
-    src: url(FiraCode-VariableFont_wght.ttf)
-}
+        @media only screen and (min-width: 1200px) {
+            .left-button {
+                width: 25%;
+                float: left;
+                position: relative;
+                left: 9%;
+            }
 
-body {
-    background-color: #333333;
-}
+            .text-block2 {
+                position: absolute;
+                bottom: 85%;
+                left: 20%;
+                background-color: rgba(0, 0, 0, 0.24);
+                color: white;
+                padding-left: 20px;
+                padding-right: 20px;
+                font-family: Firacode;
+            }
 
-a {
-    color: #ebebeb;
-    font-family: Firacode;
-}
+            .text-block {
+                position: absolute;
+                bottom: 70%;
+                left: 20%;
+                background-color: rgba(0, 0, 0, 0.24);
+                color: white;
+                padding-left: 20px;
+                padding-right: 20px;
+                font-family: Firacode;
+            }
 
-.start {
-    background-image: url(Images/Yohello.webp);
-    width: 100%;
-    height: 50%;
-    background-position: 0px -100px;
-    background-repeat: no-repeat;
-}
+            .container2 {
+                position: relative;
+                height: 800px;
+                width: 33%;
+                float: left;
+                left: 9%;
+                right: -10%;
+            }
 
-h1 {
-    font-family: FiraCode;
-    color: #EEEEEE;
-    text-align: left;
-    font-size: 60pt;
-    font-family: Firacode;
-}
+            .Contactinfo {
+                float: left;
+                width: 40%;
+            }
 
-h2 {
-    font-family: FiraCode;
-    color: #ebebeb;
-    text-align: center;
-    font-size: 20pt;
-    font-family: Firacode;
-}
+            .start {
+                padding-bottom: 10px;
+                padding-top: 10px;
+            }
 
-img {
-    object-fit: cover;
-}
+            .left-col,
+            .right-col {
+                text-align: center;
+                width: 33.33%;
+                float: left;
+            }
 
-.left-col, .right-col {
-    text-align: center;
-    width: 33.33%;
-    float: left;
-}
+            .center-col {
+                text-align: center;
+                width: 33.33%;
+                float: left;
 
-.center-col {
-    text-align: center;
-    width: 33.33%;
-    float: left;
-}
+            }
 
-p {
-    text-align: left;
-    font-family: Firacode;
-}
+            body {
+                background-color: #333333;
+            }
 
-.left-button {
-    width: 25%;
-    float: left;
-    position: relative;
-    left: 9%;
-}
+            a {
+                color: #ebebeb;
+                font-family: Firacode;
+            }
 
-.text-block2 {
-    position: absolute;
-    bottom: 85%;
-    left: 20%;
-    background-color: rgba(0, 0, 0, 0.24);
-    color: white;
-    padding-left: 20px;
-    padding-right: 20px;
-    font-family: Firacode;
-}
 
-.text-block {
-    position: absolute;
-    bottom: 70%;
-    left: 20%;
-    background-color: rgba(0, 0, 0, 0.24);
-    color: white;
-    padding-left: 20px;
-    padding-right: 20px;
-    font-family: Firacode;
-}
+            h1 {
+                font-family: FiraCode;
+                color: #EEEEEE;
+                text-align: left;
+                font-size: 60pt;
+                font-family: Firacode;
+            }
 
-.container2 {
-    position: relative;
-    height: 800px;
-    width: 33%;
-    float: left;
-    left: 9%;
-    right: -10%;
-}
+            h2 {
+                font-family: FiraCode;
+                color: #ebebeb;
+                text-align: center;
+                font-size: 20pt;
+                font-family: Firacode;
+            }
 
-h4 {
-    color: #111111;
-    text-align: center;
-    font-family: Firacode;
-}
+            img {
+                object-fit: cover;
+            }
+        }
 
-.container3 {
-    position: relative;
-    height: 380px;
-    width: 50%;
-    float: right;
-    right: 5%;
-}
+        p {
+            text-align: left;
+            font-family: Firacode;
+        }
 
-.container4 {
-    position: relative;
-    top: 40px;
-    height: 380px;
-    width: 50%;
-    float: right;
-    right: 5%;
-}
+        .left-button {
+            width: 25%;
+            float: left;
+            position: relative;
+            left: 9%;
+        }
 
-.h31 {
-    font-family: FiraCode;
-    color: #ebebeb;
-    text-align: left;
-    padding-left: 5%;
-    font-size: 18pt;
-    font-family: Firacode;
-}
+        .text-block2 {
+            position: absolute;
+            bottom: 85%;
+            left: 20%;
+            background-color: rgba(0, 0, 0, 0.24);
+            color: white;
+            padding-left: 20px;
+            padding-right: 20px;
+            font-family: Firacode;
+        }
 
-.rectangle {
-    position: relative;
-    top: -10px;
-    height: 5px;
-    width: 100px;
-    background-color: tomato;
-}
+        .text-block {
+            position: absolute;
+            bottom: 70%;
+            left: 20%;
+            background-color: rgba(0, 0, 0, 0.24);
+            color: white;
+            padding-left: 20px;
+            padding-right: 20px;
+            font-family: Firacode;
+        }
 
-.introtext {
-    float: left;
-    width: 40%;
-}
+        .container2 {
+            position: relative;
+            height: 800px;
+            width: 33%;
+            float: left;
+            left: 9%;
+            right: -10%;
+        }
 
-p {
-    font-family: FiraCode;
-    color: whitesmoke;
-    line-height: 30px;
-    font-size: 15pt;
-}
+        h4 {
+            color: #111111;
+            text-align: center;
+            font-family: Firacode;
+        }
 
-.nextpoint {
-    float: left;
-    width: 100%;
-    height: 200px;
-}
+        .container3 {
+            position: relative;
+            height: 380px;
+            width: 50%;
+            float: right;
+            right: 5%;
+        }
 
-.cgivfxtext {
-    float: right;
-    width: 40%;
-}
+        .container4 {
+            position: relative;
+            top: 40px;
+            height: 380px;
+            width: 50%;
+            float: right;
+            right: 5%;
+        }
 
-.rectangle2 {
-    float: right;
-    position: relative;
-    top: -10px;
-    height: 5px;
-    width: 100px;
-    background-color: tomato;
-}
+        .h31 {
+            font-family: FiraCode;
+            color: #ebebeb;
+            text-align: left;
+            padding-left: 5%;
+            font-size: 18pt;
+            font-family: Firacode;
+        }
 
-h3 {
-    font-family: FiraCode;
-    color: #ebebeb;
-    text-align: right;
-    padding-right: 5%;
-    font-size: 18pt;
-    font-family: Firacode;
-}
+        .rectangle {
+            position: relative;
+            top: -10px;
+            height: 5px;
+            width: 100px;
+            background-color: tomato;
+        }
 
-.rectangle3 {
-    float: right;
-    position: relative;
-    top: -10px;
-    height: 5px;
-    width: 100px;
-    background-color: rgb(235, 144, 59);
-}
+        .introtext {
+            float: left;
+            width: 40%;
+        }
 
-img {
-    float: left;
-    width: 50%;
-    height: auto;
-}
+        p {
+            font-family: FiraCode;
+            color: whitesmoke;
+            line-height: 30px;
+            font-size: 15pt;
+        }
 
-.Contactinfo {
-    float: left;
-    width: 40%;
-}
+        .nextpoint {
+            float: left;
+            width: 100%;
+            height: 200px;
+        }
 
-h3 {
-    font-family: FiraCode;
-    color: #ebebeb;
-    text-align: left;
-    padding-right: 5%;
-    font-size: 18pt;
-    font-family: Firacode;
-}
+        .cgivfxtext {
+            float: right;
+            width: 40%;
+        }
 
-</style>
+        .rectangle2 {
+            float: right;
+            position: relative;
+            top: -10px;
+            height: 5px;
+            width: 100px;
+            background-color: tomato;
+        }
+
+        h3 {
+            font-family: FiraCode;
+            color: #ebebeb;
+            text-align: right;
+            padding-right: 5%;
+            font-size: 18pt;
+            font-family: Firacode;
+        }
+
+        .rectangle3 {
+            float: right;
+            position: relative;
+            top: -10px;
+            height: 5px;
+            width: 100px;
+            background-color: rgb(235, 144, 59);
+        }
+
+        img {
+            float: left;
+            width: 50%;
+            height: auto;
+        }
+
+        .Contactinfo {
+            float: left;
+            width: 40%;
+        }
+
+        h3 {
+            font-family: FiraCode;
+            color: #ebebeb;
+            text-align: left;
+            padding-right: 5%;
+            font-size: 18pt;
+            font-family: Firacode;
+        }
+    </style>
 
 
     <div class="start">
-        <br>
-        <br>
-        <br>
         <h1>Yohello</h1>
-        <br>
-        <br>
-        <br>
     </div>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
     <div class="row">
 
         <div class="left-col">
@@ -300,17 +332,17 @@ h3 {
         </div>
     </div>
 
-<div class="nextpoint"> </div>
+    <div class="nextpoint"> </div>
 
 
 
-<div class="introtext">
-    <h3 class="h31">About Me</h3>
-    <div class="rectangle"></div>
+    <div class="introtext">
+        <h3 class="h31">About Me</h3>
+        <div class="rectangle"></div>
 
-<?php
-echo "<p> I'm Yohello from <i>the mythical land of Canada</i>, I know a bit about computers, and stuff. As of now I live I'm in the Eastern Time Zone (-4 GMT, -5 UTC), I'm avaliable  $time, I rarly know what is going on in advanced. I'm not taking job requets no matter who you are, I can redirect you to someone who might be able to. Also I know a bit about how computers work but I'm no expert, I learned <b>All</b> of it online, and self taught. I've learned Cgi and Vfx with Blender, I started programing by learning HTML, then diving straight into C# with Unity, and Python then I took a break for about a year and got back into coding with C, and that is how I got here.";
-?>
+        <?php
+        echo "<p> I'm Yohello from <i>the mythical land of Canada</i>, I know a bit about computers, and stuff. As of now I live I'm in the Eastern Time Zone (-4 GMT, -5 UTC), I'm avaliable  $time, I rarly know what is going on in advanced. I'm not taking job requets no matter who you are, I can redirect you to someone who might be able to. Also I know a bit about how computers work but I'm no expert, I learned <b>All</b> of it online, and self taught. I've learned Cgi and Vfx with Blender, I started programing by learning HTML, then diving straight into C# with Unity, and Python then I took a break for about a year and got back into coding with C, and that is how I got here.";
+        ?>
     </div>
     <br>
     <div class="nextpoint"></div>
