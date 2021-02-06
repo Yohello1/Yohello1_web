6d0f07c3e8747411ca4ada894ce633c13dd8d14e
Yohello1 <mp2702737@gmail.com>
2021-02-05 22:38:18 -0500

    testing mail

---
+++ Randomstuff/Homepage.php
@@ -0,0 +1,33 @@
+<body>
+  <h1>Test</h1>
+
+  <form method="get" action="contact.php" method="post">
+
+    <label for="name">Name</label>
+
+    <input type="name" id="name" name="name" />
+    <input type="mail" id="mail" name="mail" />
+    <input type="stuff" id="stuff" name="stuff" />
+
+    <input type="submit" value="Submit" />
+
+  </form>
+
+  <?php
+    echo $_GET["name"];
+
+
+
+
+
+
+  ?>
+</body>
+  <!-- <script>
+    var form = document.getElementById("name");
+    form.onsubmit = function (e) {
+        e.preventDefault();
+        console.log(form.name.value);
+        form.reset();
+    };
+  </script> -->
---
+++ Randomstuff/contact.php
@@ -0,0 +1,25 @@
+<?php
+if(isset($_POST['submit'])){
+    $name = $$_POST['name'];
+    $mail = $$_POST['mail'];
+    $stuff = $$_POST['stuff'];
+
+
+
+    $MailTo = "info@yohello.ca";
+    $headers = "From: Website". $mail;
+    $subject = "subject";
+    $txt = "You have gotten a message from: ".$name.".\n\n".$message;
+
+    mail($mailTo, $subject, $txt, $headers);
+    header("Location:index.php?mailsend");
+}
+
+
+
+
+
+
+
+
+?>
