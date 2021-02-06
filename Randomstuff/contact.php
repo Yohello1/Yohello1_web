cfa331571215a7b82673997938de15f8f86f4cb9
Yohello1 <mp2702737@gmail.com>
2021-02-05 23:34:54 -0500

    test5

--- Randomstuff/contact.php
+++ Randomstuff/contact.php
@@ -1,17 +1,16 @@
 <?php
-if(isset($_POST['submit'])){
-    $name = $$_POST['name'];
-    $mail = $$_POST['mail'];
-    $stuff = $$_POST['stuff'];
+if (isset($_POST['submit'])) {
+    $name =  $_POST['name'];
+    $mail =  $_POST['mail'];
+    $stuff = $_POST['stuff'];
 
 
 
-    $MailTo = "info@yohello.ca";
-    $headers = "From: Website". $mail;
+    $MailTo = "not going to dox";
+    $headers = "From: Website" . $mail;
     $subject = "subject";
-    $txt = "You have gotten a message from: ".$name.".\n\n".$message;
+    $txt = "You have gotten a message from: " . $name . ".\n\n" . $message;
 
     mail("info@yohello.ca", "testing php mail", "Body of test php mail() function");
-    header("Location:../index.php?mailsend");
+    // header("Location:../index.php?mailsend");
 }
-?>
