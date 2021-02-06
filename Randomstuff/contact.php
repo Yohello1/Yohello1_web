69b49203e706ab81c84ed72632f492c09234a745
Yohello1 <mp2702737@gmail.com>
2021-02-06 00:23:30 -0500

    test10

--- Randomstuff/contact.php
+++ Randomstuff/contact.php
@@ -1,16 +1,22 @@
 <?php
 if (isset($_POST['submit'])) {
-    $name =  $_POST['name'];
-    $mail =  $_POST['mail'];
-    $stuff = $_POST['stuff'];
+    // $name =  $_POST['name'];
+    // $mail =  $_POST['mail'];
+    // $stuff = $_POST['stuff'];
 
 
 
-    $MailTo = "not going to dox";
-    $headers = "From: Website" . $mail;
-    $subject = "subject";
-    $txt = "You have gotten a message from: " . $name . ".\n\n" . $stuff;
+    // $MailTo = "not going to dox";
+    // $headers = "From: Website" . $mail;
+    // $subject = "subject";
+    // $txt = "You have gotten a message from: " . $name . ".\n\n" . $stuff;
 
-    mail("info@yohello.ca", "testing php mail", "Body of test php mail() function");
-    // header("Location:../index.php?mailsend");
+    // mail("info@yohello.ca", "testing php mail", "Body of test php mail() function");
+    // // header("Location:../index.php?mailsend");
+ 
+$to_email = 'yahallo1234@yohello.ca';
+$subject = 'Testing PHP Mail';
+$message = 'This mail is sent using the PHP mail function';
+$headers = 'From: noreply @ company . com';
+mail($to_email,$subject,$message,$headers);
 }
