1108e27a575c3422507d1ec8a59888df98ce1c4e
Yohello1 <mp2702737@gmail.com>
2021-05-28 23:24:21 -0400

    Added CSS file for XCC

--- XCC/privacy.php
+++ XCC/privacy.php
@@ -2,8 +2,7 @@
     <title>Xavier Coding Club</title>
     <link rel="preconnect" href="https://fonts.gstatic.com" />
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100&display=swap" rel="stylesheet" />
-    <link href="../index.css" rel="stylesheet" type="text/css" />
-    <link rel="icon" href="../images/elogo_1.png">
+    <link href="index.css" rel="stylesheet" type="text/css" />
 </head>
 
 
