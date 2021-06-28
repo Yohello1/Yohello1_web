a5eae986eb3f6a456369bd6e612be6265316954a
Yohello1 <mp2702737@gmail.com>
2021-06-28 02:35:09 -0400

    Finished look of the project page

--- Projects/index.php
+++ Projects/index.php
@@ -1,35 +1,67 @@
 <head>
-    <title>Project</title>
-	<link rel="stylesheet" href="../indexx.css">
+  <title>Project</title>
+  <link rel="stylesheet" href="../indexx.css">
 </head>
 
 <style>
-  h1{
+  h1 {
     text-align: center;
     padding-top: 100px;
+    opacity: 1;
   }
 
-  .line{
+  h3 {
+    opacity: 1;
+    text-align: center;
+    font-size: 20pt;
+  }
+
+  p {
+    text-align: center;
+  }
+
+  .line {
     height: 5px;
     background-color: white;
   }
 
-  .section_title{
-    width:49.8%;
+  .section_title {
+    width: 49.8%;
   }
-  #code{
-    float:left;
+
+  #code {
+    float: left;
   }
-  #cgi{
-    float:right;
+
+  #cgi {
+    float: right;
   }
-  .divider{
+
+  .divider {
     background-color: white;
     width: 0.4%;
     height: 500px;
-    float:left;
+    float: left;
   }
 
+  .pics {
+    height: 480px;
+    width: 100%;
+    float: left;
+    background-repeat: no-repeat;
+    background-size: contain;
+    background-position: center;
+  }
+
+  .dark_box {
+    background-color: rgba(.1, .1, .1, .5);
+    width: 40%;
+    margin: auto;
+  }
+
+  .words {
+    text-align: center;
+  }
 </style>
 
 <body>
@@ -37,12 +69,26 @@
   <div class="line"></div>
 
   <div class="section_title" id="code">
-    <h2>Cgi</h2>
+    <h2>Things with pictures!!!!</h2>
+    <div class="pics" style="background-image: url('https://placewaifu.com/image/200');">
+      <div class="dark_box">
+        <h3> [Project name] </h3>
+        <p> [small description] </p>
+      </div>
+    </div>
   </div>
+
   <div class="divider">
   </div>
 
   <div class="section_title" id="cgi">
-    <h2>Code</h2>
+    <h2>Things without pictures</h2>
+    <div class="cgi">
+      <div class="words">
+        <h3>[Project Name]</h3>
+        <p>[Short descrptions]</p>
+        <p><a href="[link]">[Website] </a></p>
+      </div>
+    </div>
   </div>
 </body>
