67e75750efd503dde27abcc2412a780d2b9a1ab2
Yohello1 <mp2702737@gmail.com>
2022-08-03 04:45:08 -0400

    First commit in a forever

---
+++ test.php
@@ -0,0 +1,88 @@
+<head>
+
+    <meta charset="UTF-8">
+
+    <title>Yohwllo</title>
+    <link rel="shortcut icon" type="image/x-icon" href="favicon.ico" />
+</head>
+
+<style>
+    body {
+        background-color: #161616;
+    }
+
+    .header_one {
+        background-color: #FFFFFF;
+        position: fixed;
+        width: 750px;
+        height: 100px;
+        top: 10px;
+        left: 50%;
+        transform: translate(-50%, 0);
+        margin-right: 10px;
+
+    }
+
+    .header_two {
+        background-color: #FFFFFF;
+        position: fixed;
+        width: 750px;
+        height: 100px;
+        top: 20px;
+        left: 50%;
+        transform: translate(-50%, 0);
+        margin-left: 10px;
+    }
+
+    .header_three {
+        background-color: #161616;
+        position: fixed;
+        width: 740px;
+        height: 90px;
+        top: 20px;
+        left: 50%;
+        transform: translate(-50%, 0);
+        margin-left: 5px;
+    }
+
+    .text_title {
+        text-align: center;
+        vertical-align: middle;
+        line-height: 20px;
+        color: #FFFFFF;
+        font-size: 25px;
+    }
+
+    .left_top_title {
+        color: #FFFFFF;
+        position: relative;
+        left: 5px;
+        top:-53px;
+
+    }
+    .left_bot_title {
+        color: #FFFFFF;
+        position: relative;
+        left: 5px;
+        top:-70px;
+        font-size: 10px;
+    }
+</style>
+
+<body>
+    <!-- <p> Hello there </p> -->
+    <div class="header_one"></div>
+    <div class="header_two"></div>
+    <div class="header_three">
+        <h1 class="text_title">
+            My Waste Of Space
+        </h1>
+        <h2 class="left_top_title">
+            Yohwllo.ca
+        </h2>
+        <h2 class="left_bot_title">
+            I got bored
+        </h2>
+
+    </div>
+</body>
