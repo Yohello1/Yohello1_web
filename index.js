c9c051184cfdd6df8fc170ed5157bed6bf208ccf
Yohwllo <mp2702737@gmail.com>
2024-03-24 00:39:23 -0400

    Shitty blog page created, not done

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const app = express()
 const port = 8080
 
 
-
 // Static files
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
@@ -13,14 +12,35 @@ app.set('view engine', 'ejs');
 // Pages & Requests
 app.get('/', (req, res) =>
 {
-    res.render('index', {text: "world"});
+    res.render('index', {text: "<h1> HII </h1>"});
 })
 
-app.get('/blog' (req,res) =>{
-    res.render("blog");
+app.get('/blog', (req,res) => {
+    var bannerCode = "";
+    var bannerCSS = "";
+    try{
+        bannerCode = fs.readFileSync('internal/banner.html', 'utf8');
+    }
+    catch(err)
+    {
+        console.log(err);
+    }
+
+    try{
+        bannerCSS = fs.readFileSync('internal/banner.css', 'utf8');
+    }
+    catch(err)
+    {
+        console.log(err);
+    }
+
+    res.render("blog", {bannerCode: bannerCode, bannerCSS: bannerCSS});
 })
 
 
 app.listen(port, () => {
+
+
+
   console.log(`Example app listening on port ${port}`)
 })
