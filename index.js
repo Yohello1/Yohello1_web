180de1f7ad085959f5f91135bfa74e58a3abd3e5
Yohwllo <mp2702737@gmail.com>
2024-04-17 01:03:27 -0400

    I was CRYING this morning

--- index.js
+++ index.js
@@ -37,7 +37,18 @@ app.get('/blog', (req,res) => {
         console.log(err);
     }
 
-    res.render("blog", {bannerCode: bannerCode, bannerCSS: bannerCSS});
+    let linksToFiles = fs.readdirSync("public/myPosts");
+
+    console.log(linksToFiles);
+
+    var str = "";
+
+    linksToFiles.forEach(function(slide)
+    {
+        str += '<a href=myPosts/' + slide + '>' + slide + '</a> <br>';
+    });
+
+    res.render("blog", {bannerCode: bannerCode, bannerCSS: bannerCSS, links: str});
 })
 
 app.get('/projects', (req, res) =>{
@@ -60,41 +71,6 @@ app.get('/projects', (req, res) =>{
     }
 
     res.render("projects", {bannerCode: bannerCode, bannerCSS: bannerCSS});
-})
-
-
-app.get('/blogEdit', (req, res) =>{
-    var bannerCode = "";
-    var bannerCSS = "";
-    try{
-        bannerCode = fs.readFileSync('internal/banner.html', 'utf8');
-    }
-    catch(err)
-    {
-        console.log(err);
-    }
-
-    try{
-        bannerCSS = fs.readFileSync('internal/banner.css', 'utf8');
-    }
-    catch(err)
-    {
-        console.log(err);
-    }
-
-    res.render("blogEdit",  {bannerCode: bannerCode, bannerCSS: bannerCSS});
-})
-
-app.post('/blogPost', (req, res) => {
-    var textt = req['body']['text'];
-    var title = req['body']['title'];
-
-    console.log(textt);
-
-    var decrypted = CryptoJS.AES.decrypt(textt, "ELjiJ5/njYSYYKJI1GSYY6mV7bhhQdgwGwXj7TCQSATsRsjHQMYPVY/+hG1tWV0f");
-    var finalText = decrypted.toString(CryptoJS.enc.Utf8);
-    console.log(finalText);
-
 })
 
 
